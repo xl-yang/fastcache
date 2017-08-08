ea6f35f1ade041bf9b32c0eddc62cd64f5914740
leo <leo>
2017-08-09 00:19:05 +0800

    update

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
 'use strict'
 
 var cacheStore = {};
+var cacheInd = true;
 
 function Cache() {
 }
 
 Cache.prototype.get = function (key) {
-    var value = cacheStore[key];
-    return value;
+    return get(key);
 }
 
 Cache.prototype.set = function (key, value) {
-    cacheStore[key] = value;
+    set(key, value);
 }
 
 Cache.prototype.del = function (key) {
-    delete cacheStore[key];
+    del(key);
 }
 
 // return cacheStore object that contains all the keys and values 
 Cache.prototype.all = function () {
-    return cacheStore;
+    return all();
 }
 
 Cache.prototype.reset = function () {
+    reset();
+}
+
+Cache.prototype.switchToCache = function () {
+    cacheInd = true;
+}
+
+Cache.prototype.switchToFile = function () {
+    cacheInd = false;
+}
+
+function get(key) {
+    var value = cacheStore[key];
+    return value;
+}
+
+function set(key, value) {
+    cacheStore[key] = value;
+}
+
+function del(key) {
+    delete cacheStore[key];
+}
+
+function all() {
+    return cacheStore;
+}
+
+function reset() {
     cacheStore = {};
 }
 
+
 module.exports = new Cache();
+
